404e0271437bb45d1fe688cf213905a652f5162f
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-02-26 19:35:52 +1200

    JpaUsersTest tests actually running

--- src/test/java/com/example/jpaproject4/JpaUsersTest.java
+++ src/test/java/com/example/jpaproject4/JpaUsersTest.java
@@ -8,7 +8,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+//
 import java.time.LocalDate;
 import java.util.List;
 
